3adcfae92a8bf81f7dd53f42d0143df6f476f946
Paul Kristoff <Paul.Kristoff@kristoffs.com>
2014-11-28 13:05:27 -0500

    RAILS_ENV = production

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+puts "before init. ENV['RAILS_ENV'] = #{ENV['RAILS_ENV']}"
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+puts 'after init.'
